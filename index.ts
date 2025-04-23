9cc7380cf17ac06017b1b55d0ab87d77e82b6971
Uday Raj <udayraj.vadeghar@gmail.com>
2025-04-23 12:06:51 +0530

    Add new tools for fetching balance and placing/selling stock orders in the MCP server. Removed unused tools for cleaner code.

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod";
-import { getProfile } from "./trade";
+import { getBalance, getProfile, placeOrder, sellOrder } from "./trade";
 
 const server = new McpServer({
   name: "Demo",
@@ -30,9 +30,24 @@ server.tool(
 );
 
 server.tool(
+  "get-balance",
+  "This funciton is to fetch the equity balance from zerodha",
+  async () => {
+    const balance = await getBalance();
+    return {
+      content: [
+        { type: "text", text: "Balance fetched successfully" + balance },
+      ],
+    };
+  }
+);
+
+server.tool(
   "buy-stock",
+  "This is to buy the stock in the zerodha ,buy-order will be placed on the stock",
   { stock: z.string(), qty: z.number() },
   async ({ stock, qty }) => {
+    await placeOrder({ stock, qty });
     return {
       content: [{ type: "text", text: "Bought the stock Successfully" }],
     };
@@ -40,35 +55,21 @@ server.tool(
 );
 
 server.tool(
-  "add-two-numbers",
-  { a: z.number(), b: z.number() },
-  async ({ a, b }) => ({
-    content: [{ type: "text", text: String(a + b) }],
-  })
-);
-
-server.tool("factorial", { a: z.number() }, async ({ a }) => {
-  let result = 1;
-  for (let i = 2; i <= a; i++) {
-    result *= i;
+  "sell-stock",
+  "This is to sell the stock in the zerodha , sell-order will be placed on the stock",
+  { stock: z.string(), qty: z.number() },
+  async ({ stock, qty }) => {
+    await sellOrder({ stock, qty });
+    return {
+      content: [{ type: "text", text: "Sold the stock Successfully" }],
+    };
   }
-  return {
-    content: [{ type: "text", text: String(result) }],
-  };
-});
-
-server.tool("async-task", async () => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({ content: [{ type: "text", text: "Task completed" }] });
-    }, 1000);
-  });
-});
+);
 
 async function main() {
   const transport = new StdioServerTransport();
   await server.connect(transport);
-  // console.log(await getProfile());
+
   console.error("MCP server is connected and running.");
   await new Promise(() => {});
 }
